30d49a9b133b6bdc13c93fdb42acecb3dfceaaef
LongYihao <3210101621@zju.edu.cn>
2023-03-12 15:37:33 +0800

    代码写完了，bug一堆

--- LinuxRelease/SDK/c++/CMakeFiles/main.dir/compiler_depend.make
+++ LinuxRelease/SDK/c++/CMakeFiles/main.dir/compiler_depend.make
@@ -181,8 +181,117 @@ CMakeFiles/main.dir/main.cpp.o: main.cpp \
   /usr/include/c++/11/bits/ostream.tcc \
   /usr/include/c++/11/istream \
   /usr/include/c++/11/bits/istream.tcc \
-  distance.h
+  /usr/include/c++/11/cmath \
+  /usr/include/math.h \
+  /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+  /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+  /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+  /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+  /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+  /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+  /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+  /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+  /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+  /usr/include/c++/11/bits/specfun.h \
+  /usr/include/c++/11/limits \
+  /usr/include/c++/11/tr1/gamma.tcc \
+  /usr/include/c++/11/tr1/special_function_util.h \
+  /usr/include/c++/11/tr1/bessel_function.tcc \
+  /usr/include/c++/11/tr1/beta_function.tcc \
+  /usr/include/c++/11/tr1/ell_integral.tcc \
+  /usr/include/c++/11/tr1/exp_integral.tcc \
+  /usr/include/c++/11/tr1/hypergeometric.tcc \
+  /usr/include/c++/11/tr1/legendre_function.tcc \
+  /usr/include/c++/11/tr1/modified_bessel_func.tcc \
+  /usr/include/c++/11/tr1/poly_hermite.tcc \
+  /usr/include/c++/11/tr1/poly_laguerre.tcc \
+  /usr/include/c++/11/tr1/riemann_zeta.tcc \
+  distance.h \
+  /usr/include/c++/11/vector \
+  /usr/include/c++/11/bits/stl_uninitialized.h \
+  /usr/include/c++/11/bits/stl_vector.h \
+  /usr/include/c++/11/bits/stl_bvector.h \
+  /usr/include/c++/11/bits/vector.tcc \
+  /usr/include/c++/11/algorithm \
+  /usr/include/c++/11/utility \
+  /usr/include/c++/11/bits/stl_relops.h \
+  /usr/include/c++/11/bits/stl_algo.h \
+  /usr/include/c++/11/bits/algorithmfwd.h \
+  /usr/include/c++/11/bits/stl_heap.h \
+  /usr/include/c++/11/bits/stl_tempbuf.h \
+  /usr/include/c++/11/bits/uniform_int_dist.h \
+  /usr/include/c++/11/pstl/glue_algorithm_defs.h \
+  /usr/include/c++/11/functional \
+  /usr/include/c++/11/tuple \
+  /usr/include/c++/11/array \
+  /usr/include/c++/11/bits/uses_allocator.h \
+  /usr/include/c++/11/bits/invoke.h \
+  /usr/include/c++/11/bits/refwrap.h \
+  /usr/include/c++/11/bits/std_function.h \
+  /usr/include/c++/11/unordered_map \
+  /usr/include/c++/11/ext/aligned_buffer.h \
+  /usr/include/c++/11/bits/hashtable.h \
+  /usr/include/c++/11/bits/hashtable_policy.h \
+  /usr/include/c++/11/bits/enable_special_members.h \
+  /usr/include/c++/11/bits/node_handle.h \
+  /usr/include/c++/11/bits/unordered_map.h \
+  /usr/include/c++/11/bits/erase_if.h \
+  /usr/include/c++/11/pstl/execution_defs.h
 
+
+/usr/include/c++/11/bits/erase_if.h:
+
+/usr/include/c++/11/bits/enable_special_members.h:
+
+/usr/include/c++/11/bits/hashtable_policy.h:
+
+/usr/include/c++/11/bits/hashtable.h:
+
+/usr/include/c++/11/bits/refwrap.h:
+
+/usr/include/c++/11/bits/uses_allocator.h:
+
+/usr/include/c++/11/array:
+
+/usr/include/c++/11/bits/stl_heap.h:
+
+/usr/include/c++/11/bits/algorithmfwd.h:
+
+/usr/include/c++/11/bits/stl_relops.h:
+
+/usr/include/c++/11/bits/vector.tcc:
+
+/usr/include/c++/11/bits/stl_bvector.h:
+
+/usr/include/c++/11/bits/stl_uninitialized.h:
+
+/usr/include/c++/11/vector:
+
+/usr/include/c++/11/tr1/riemann_zeta.tcc:
+
+/usr/include/c++/11/tr1/legendre_function.tcc:
+
+/usr/include/c++/11/tr1/beta_function.tcc:
+
+/usr/include/c++/11/limits:
+
+/usr/include/c++/11/bits/specfun.h:
+
+/usr/include/x86_64-linux-gnu/bits/iscanonical.h:
+
+/usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h:
+
+/usr/include/x86_64-linux-gnu/bits/mathcalls.h:
+
+/usr/include/x86_64-linux-gnu/bits/flt-eval-method.h:
+
+/usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h:
+
+/usr/include/c++/11/bits/stl_algo.h:
+
+/usr/include/x86_64-linux-gnu/bits/math-vector.h:
+
+/usr/include/math.h:
 
 /usr/include/c++/11/bits/istream.tcc:
 
@@ -198,9 +307,13 @@ CMakeFiles/main.dir/main.cpp.o: main.cpp \
 
 /usr/include/c++/11/cwctype:
 
+/usr/include/c++/11/tr1/poly_hermite.tcc:
+
 /usr/include/c++/11/bits/locale_facets.h:
 
 /usr/include/c++/11/bits/basic_ios.h:
+
+/usr/include/c++/11/tr1/bessel_function.tcc:
 
 /usr/include/c++/11/bits/streambuf.tcc:
 
@@ -208,7 +321,11 @@ CMakeFiles/main.dir/main.cpp.o: main.cpp \
 
 /usr/include/x86_64-linux-gnu/c++/11/bits/error_constants.h:
 
+/usr/include/c++/11/bits/stl_vector.h:
+
 /usr/include/c++/11/system_error:
+
+/usr/include/x86_64-linux-gnu/bits/fp-fast.h:
 
 /usr/include/c++/11/bits/locale_classes.tcc:
 
@@ -216,9 +333,13 @@ CMakeFiles/main.dir/main.cpp.o: main.cpp \
 
 /usr/include/c++/11/bits/charconv.h:
 
+/usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h:
+
 /usr/include/x86_64-linux-gnu/bits/types/error_t.h:
 
 distance.h:
+
+/usr/include/c++/11/tr1/ell_integral.tcc:
 
 /usr/include/asm-generic/errno.h:
 
@@ -227,6 +348,8 @@ distance.h:
 /usr/include/wctype.h:
 
 /usr/include/x86_64-linux-gnu/bits/errno.h:
+
+/usr/include/c++/11/utility:
 
 /usr/include/errno.h:
 
@@ -245,6 +368,8 @@ distance.h:
 /usr/include/x86_64-linux-gnu/bits/uintn-identity.h:
 
 /usr/include/x86_64-linux-gnu/bits/byteswap.h:
+
+/usr/include/c++/11/ext/aligned_buffer.h:
 
 /usr/include/endian.h:
 
@@ -302,11 +427,15 @@ distance.h:
 
 /usr/include/c++/11/bits/range_access.h:
 
+/usr/include/c++/11/tr1/poly_laguerre.tcc:
+
 /usr/include/c++/11/cstdlib:
 
 /usr/include/c++/11/bits/stl_iterator_base_types.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h:
+
+/usr/include/c++/11/bits/stl_tempbuf.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/__FILE.h:
 
@@ -344,6 +473,8 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/locale_t.h:
 
+/usr/include/c++/11/pstl/execution_defs.h:
+
 /usr/include/x86_64-linux-gnu/bits/wordsize.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h:
@@ -352,9 +483,13 @@ distance.h:
 
 /usr/include/c++/11/bits/alloc_traits.h:
 
+/usr/include/c++/11/functional:
+
 /usr/include/asm-generic/errno-base.h:
 
 /usr/include/c++/11/bits/char_traits.h:
+
+/usr/include/c++/11/bits/std_function.h:
 
 /usr/include/c++/11/bits/allocator.h:
 
@@ -377,6 +512,8 @@ distance.h:
 /usr/include/c++/11/iosfwd:
 
 /usr/include/x86_64-linux-gnu/bits/time64.h:
+
+/usr/include/c++/11/bits/node_handle.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h:
 
@@ -404,9 +541,13 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h:
 
+/usr/include/c++/11/tr1/special_function_util.h:
+
 /usr/include/x86_64-linux-gnu/bits/waitstatus.h:
 
 /usr/include/c++/11/bits/postypes.h:
+
+/usr/include/c++/11/bits/invoke.h:
 
 /usr/include/x86_64-linux-gnu/bits/timex.h:
 
@@ -422,13 +563,19 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h:
 
+/usr/include/c++/11/tuple:
+
 /usr/include/c++/11/type_traits:
+
+/usr/include/c++/11/pstl/glue_algorithm_defs.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/time_t.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h:
 
 /usr/include/c++/11/string:
+
+/usr/include/c++/11/bits/uniform_int_dist.h:
 
 /usr/include/c++/11/bits/cxxabi_init_exception.h:
 
@@ -454,6 +601,8 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/sched.h:
 
+/usr/include/c++/11/bits/unordered_map.h:
+
 /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h:
 
 /usr/include/c++/11/bits/move.h:
@@ -466,11 +615,15 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/stdint-intn.h:
 
+/usr/include/c++/11/unordered_map:
+
 /usr/include/c++/11/cctype:
 
 /usr/include/x86_64-linux-gnu/sys/select.h:
 
 /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h:
+
+/usr/include/c++/11/tr1/hypergeometric.tcc:
 
 /usr/include/c++/11/bits/localefwd.h:
 
@@ -480,7 +633,11 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/locale.h:
 
+/usr/include/c++/11/tr1/modified_bessel_func.tcc:
+
 /usr/include/x86_64-linux-gnu/bits/endianness.h:
+
+/usr/include/x86_64-linux-gnu/bits/fp-logb.h:
 
 /usr/include/c++/11/bits/ios_base.h:
 
@@ -522,9 +679,15 @@ distance.h:
 
 /usr/include/x86_64-linux-gnu/bits/setjmp.h:
 
+/usr/include/c++/11/tr1/gamma.tcc:
+
 /usr/include/c++/11/bits/locale_classes.h:
 
+/usr/include/c++/11/cmath:
+
 /usr/include/x86_64-linux-gnu/c++/11/bits/c++allocator.h:
+
+/usr/include/c++/11/algorithm:
 
 /usr/include/x86_64-linux-gnu/bits/select.h:
 
@@ -543,5 +706,7 @@ main.cpp:
 /usr/include/c++/11/bits/cxxabi_forced.h:
 
 /usr/include/c++/11/ext/alloc_traits.h:
+
+/usr/include/c++/11/tr1/exp_integral.tcc:
 
 /usr/include/c++/11/bits/stl_construct.h:
--- LinuxRelease/SDK/c++/main.cpp
+++ LinuxRelease/SDK/c++/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include<cmath>
 #include "distance.h"
+#include<vector>
+#include <algorithm>
 using namespace std;
 char line[1024];
 int money;
-extern float distance[100][100];
+extern float Distance[100][100];
+extern bool isSellWorkbench[10][10];
 int amountOfWorkbench;
+struct nearWorkbench {
+    int ID;
+    float distance;
+};
 struct workbench {
     //int ID;ID用数组序号表示
     int type;//范围[1, 9]
@@ -15,7 +23,9 @@ struct workbench {
     int frameLeft;//剩余生产时间（帧数） -1：表示没有生产。0：表示生产因输出格满而阻塞。>=0：表示剩余生产帧数。
     int material;//二进制位表描述，例如 48(110000)表示拥有物品 4 和 5。
     int target;//是否被作为了robot的抵达目的地。二进制位表描述，例如 48(110000)表示物品4和5，被预定了。
-    int product; //产品放入material一起表示
+    int product;
+    vector<nearWorkbench> nearestSellWorkbench;
+    vector<nearWorkbench> nearestWorkbench;
 };
 struct robot {
     int inWhichWorkbench;//-1：表示当前没有处于任何工作台附近  [0,工作台总数-1] ：表示某工作台的下标，从 0 开始，按输入顺序定。当前机器人的所有购买、出售行为均针对该工作台进行。
@@ -30,9 +40,28 @@ struct robot {
     float y; //坐标
     int indexX;
     int indexY;//从0开始
+    vector<int> target;
 };
 robot robots[4];
 workbench workbenchs[50];
+bool calculateDistance() {
+    for (int i = 0; i < amountOfWorkbench; i++) {
+        for (int j = 0; j < amountOfWorkbench; j++) {
+            if (j == i) continue;
+            nearWorkbench nb;
+            nb.ID = j;
+            nb.distance = Distance[abs(workbenchs[i].indexX- workbenchs[j].indexX)] [abs(workbenchs[i].indexY - workbenchs[j].indexY)];
+            workbenchs[i].nearestWorkbench.push_back(nb);
+            if(isSellWorkbench[i][j]) workbenchs[i].nearestSellWorkbench.push_back(nb);
+        }
+        std::sort(workbenchs[i].nearestWorkbench.begin(),
+            workbenchs[i].nearestWorkbench.end(),
+            [](const nearWorkbench& a, const nearWorkbench& b) {
+                return a.distance < b.distance;
+            });
+    }
+    return 1;
+}
 bool readMapUntilOK() {
     int robotIndex = 0;
     int workBenchIndex = 0;
@@ -65,6 +94,9 @@ bool readMapUntilOK() {
     }
     getchar();
     getchar();
+    calculateDistance();
+    puts("OK");
+    fflush(stdout);
     return 1;
 }
 bool readFrameUntilOK() {
@@ -80,6 +112,8 @@ bool readFrameUntilOK() {
             &((robots + i)->timeValue), &((robots + i)->crushValue),
             &((robots + i)->angularVelocity), &((robots + i)->linearVelocityX), & ((robots + i)->linearVelocityY),
             & ((robots + i)->orientation), & ((robots + i)->x), & ((robots + i)->y) );
+        robots[i].indexX = (robots[i].x - 0.25) * 2;
+        robots[i].indexY = (robots[i].y - 0.25) * 2;
     }
     while (fgets(line, sizeof line, stdin)) {
         if (line[0] == 'O' && line[1] == 'K') {
@@ -88,34 +122,145 @@ bool readFrameUntilOK() {
     }
     return 1;
 }
-bool readUntilOK() {
-    char line[1024];
-    while (fgets(line, sizeof line, stdin)) {
-        if (line[0] == 'O' && line[1] == 'K') {
-            return true;
+//bool readUntilOK() {
+//    char line[1024];
+//    while (fgets(line, sizeof line, stdin)) {
+//        if (line[0] == 'O' && line[1] == 'K') {
+//            return true;
+//        }
+//        //do something
+//    }
+//    return false;
+//}
+void findTargetForRobots() {
+
+    //为每个robot找到目标
+    for (int i = 0; i < 4; i++) {
+        if (!robots[i].target.empty()) continue;
+        int target1 = 0;int target2 = 0;
+        int minDistance = 70;
+        bool haveproduct = 0;
+        for (int j = 0 ; j < amountOfWorkbench; j++) {
+            if (workbenchs[j].target & (1 << workbenchs[j].type)) continue; //已预定则跳过
+            if (haveproduct) {
+                if (workbenchs[j].product) {
+                    int whichSellEmpty = -1;
+                    for (int whichSellEmpty_i = 0; whichSellEmpty_i < workbenchs[j].nearestSellWorkbench.size(); whichSellEmpty_i++) { //找销售方
+                        if ((workbenchs[(workbenchs[j].nearestSellWorkbench[whichSellEmpty_i].ID)].material & (1 << workbenchs[j].type)) == 0 &&
+                            (workbenchs[(workbenchs[j].nearestSellWorkbench[whichSellEmpty_i].ID)].target & (1 << workbenchs[j].type)) == 0) { //销售方无原材料且原材料未被预定，则
+                            whichSellEmpty = whichSellEmpty_i;
+                            break;
+                        }
+                    }
+                    if (whichSellEmpty == -1) continue;
+                    int tempDistance = Distance[abs(robots[i].indexX - workbenchs[j].indexX)][abs(robots[i].indexY - workbenchs[j].indexY)] + workbenchs[j].nearestSellWorkbench[whichSellEmpty].distance;
+                    if (tempDistance < minDistance) {
+                        minDistance = tempDistance;
+                        target1 = j;
+                        target2 = workbenchs[j].nearestSellWorkbench[whichSellEmpty].ID;
+                    }
+                }
+            }
+            else {
+                if (workbenchs[j].frameLeft >= 0 || workbenchs[j].product) {
+                    if (workbenchs[j].product) haveproduct = 1;
+                    int whichSellEmpty = -1;
+                    for (int whichSellEmpty_i = 0; whichSellEmpty_i < workbenchs[j].nearestSellWorkbench.size(); whichSellEmpty_i++) { //找销售方
+                        if ((workbenchs[(workbenchs[j].nearestSellWorkbench[whichSellEmpty_i].ID)].material & (1 << workbenchs[j].type)) == 0 &&
+                            (workbenchs[(workbenchs[j].nearestSellWorkbench[whichSellEmpty_i].ID)].target & (1 << workbenchs[j].type)) == 0) { //销售方无原材料且原材料未被预定，则
+                            whichSellEmpty = whichSellEmpty_i;
+                            break;
+                        }
+                    }
+                    if (whichSellEmpty == -1) continue;
+                    int tempDistance = Distance[abs(robots[i].indexX - workbenchs[j].indexX)][abs(robots[i].indexY - workbenchs[j].indexY)] + workbenchs[j].nearestSellWorkbench[whichSellEmpty].distance;
+                    if (tempDistance < minDistance) {
+                        minDistance = tempDistance;
+                        target1 = j;
+                        target2 = workbenchs[j].nearestSellWorkbench[whichSellEmpty].ID;
+                    }
+                }
+
+            }
         }
-        //do something
+        workbenchs[target1].target |= (1 << workbenchs[target1].type);//预定
+        workbenchs[target2].target |= (1 << workbenchs[target1].type);
+        robots[i].target.push_back(target1); robots[i].target.push_back(target2);
     }
-    return false;
+}
+
+bool outputCommand(int _frameID) {
+    printf("%d\n", _frameID);
+
+    //买卖
+    for (int i = 0; i < 4; i++) { 
+        if (robots[i].inWhichWorkbench < 0) continue;
+        if (workbenchs[robots[i].inWhichWorkbench].type == robots[i].productInHand) {
+            printf("sell %d\n", i);
+            robots[i].target.clear();
+        }
+        if (workbenchs[robots[i].inWhichWorkbench].product|| robots[i].productInHand==0) {
+            printf("buy %d\n", i);
+            for (int whichSellEmpty_i = 0; whichSellEmpty_i < workbenchs[robots[i].inWhichWorkbench].nearestSellWorkbench.size(); whichSellEmpty_i++) { //找销售方
+                if ((workbenchs[(workbenchs[robots[i].inWhichWorkbench].nearestSellWorkbench[whichSellEmpty_i].ID)].material & (1 << workbenchs[robots[i].inWhichWorkbench].type)) == 0 &&
+                    (workbenchs[(workbenchs[robots[i].inWhichWorkbench].nearestSellWorkbench[whichSellEmpty_i].ID)].target & (1 << workbenchs[robots[i].inWhichWorkbench].type)) == 0) { //销售方无原材料且原材料未被预定，则
+                    robots[i].target.push_back(workbenchs[robots[i].inWhichWorkbench].nearestSellWorkbench[whichSellEmpty_i].ID);
+                    break;
+                }
+            }
+            if(robots[i].target.empty())    robots[i].target.push_back(workbenchs[robots[i].inWhichWorkbench].nearestSellWorkbench[0].ID);
+        }
+    }
+
+    findTargetForRobots();
+
+    for (int robotId = 0; robotId < 4; robotId++) {
+        float targetOritation;
+        if (workbenchs[robots[robotId].target[0]].x - robots[robotId].x == 0) {
+            targetOritation = ((workbenchs[robots[robotId].target[0]].y > robots[robotId].y) ? (3.1415926 / 2) : ( - 3.1415926 / 2) );
+        }
+        else {
+            targetOritation = atan((workbenchs[robots[robotId].target[0]].y - robots[robotId].y) / (workbenchs[robots[robotId].target[0]].x - robots[robotId].x));
+            if (workbenchs[robots[robotId].target[0]].x < robots[robotId].x) {
+                if (targetOritation < 0)
+                    targetOritation = -3.1515926 - targetOritation;
+                else
+                    targetOritation = 3.1515926 - targetOritation;
+            }
+        }
+        double angleSpeed = (targetOritation - robots[robotId].orientation) * 20;
+        float lineSpeed = Distance[abs(robots[robotId].indexX- workbenchs[robots[robotId].target[0]].indexX)] [abs(robots[robotId].indexY - workbenchs[robots[robotId].target[0]].indexY)];
+        if (lineSpeed >= 0 && lineSpeed < 0.5) {
+            lineSpeed = 0.5;
+        }
+        else if (lineSpeed < 0 && lineSpeed > -0.5) {
+            lineSpeed = -0.5;
+        }
+        printf("forward %d %f\n", robotId, 0.5); //lineSpeed  测试
+        printf("rotate %d %f\n", robotId, angleSpeed);
+        //fprintf(stderr, "forward %d %d\n", robotId, lineSpeed);
+        //fprintf(stderr, "rotate %d %f\n", robotId, angleSpeed);
+    }
+    //////////////////////////////////
+    int debugi = 1;
+    if (!robots[debugi].target.empty()) {
+        fprintf(stderr, "target %d type %d", robots[debugi].target[0],workbenchs[robots[debugi].target[0]].type);
+        if(robots[debugi].target.size()>1) fprintf(stderr, "    target %d type %d", robots[debugi].target[1], workbenchs[robots[debugi].target[1]].type);
+        fprintf(stderr, "\n");
+        fprintf(stderr, "x y %f %f\n", robots[debugi].x, robots[debugi].y);
+    }
+    //////////////////////////////////
+    printf("OK\n");
+    fflush(stdout);
+    return 1;
 }
 
 int main() {
     readMapUntilOK();
-    puts("OK");
-    fflush(stdout);
     int frameID;
     while (scanf("%d", &frameID) != EOF) {
         readFrameUntilOK();
-        //readUntilOK();
-        printf("%d\n", frameID);
-        int lineSpeed = 3;
-        double angleSpeed = 1.5;
-        for (int robotId = 0; robotId < 4; robotId++) {
-            printf("forward %d %d\n", robotId, lineSpeed);
-            printf("rotate %d %f\n", robotId, angleSpeed);
-        }
-        printf("OK\n", frameID);
-        fflush(stdout);
+        outputCommand(frameID);
     }
     return 0;
 }
